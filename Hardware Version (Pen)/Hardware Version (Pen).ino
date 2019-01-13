791b844cbfc560a781804e8ee1333a18638fd506
Jing Yao <jinghaoyao_@hotmail.com>
2019-01-13 14:36:55 -0500

    Update Hardware Version (Pen).ino

--- Hardware Version (Pen)/Hardware Version (Pen).ino
+++ Hardware Version (Pen)/Hardware Version (Pen).ino
@@ -43,6 +43,7 @@ void loop(){
 if(digitalRead(buttonPin) == LOW){
   while(digitalRead(buttonPin) == LOW){
   }
+  numSeconds = 0;
   buttonPressed = !buttonPressed;
 }
 
@@ -55,7 +56,7 @@ if(buttonPressed == false){
           tone(buzzer, NOTE_D5, 1000);
       }
     }
-    if(millis() - lastServoTurn >= 20){
+    if(millis() - lastServoTurn >= 50){
       if(servoPos == 0){
         servoPos = 180;
       }
@@ -138,42 +139,3 @@ bool isPencilMoving(double currRoll0, double currPitch0, double oldRoll0, double
     return false;
   }
 }
-
-void SongOfStorms(){
-  int melody[]{
-  NOTE_D5, NOTE_F5, NOTE_D6,
-  NOTE_D5, NOTE_F5, NOTE_D6,
-  NOTE_E6, NOTE_F6, NOTE_E6, NOTE_F6, NOTE_E6, NOTE_C6, NOTE_A5,
-  NOTE_A5, NOTE_D5, NOTE_F5, NOTE_G5, NOTE_A5,
-  NOTE_A5, NOTE_D5, NOTE_F5, NOTE_G5, NOTE_E5,
-  NOTE_D5, NOTE_F5, NOTE_D6,
-  NOTE_D5, NOTE_F5, NOTE_D6,
-  NOTE_E6, NOTE_F6, NOTE_E6, NOTE_F6, NOTE_E6, NOTE_C6, NOTE_A5,
-  NOTE_A5, NOTE_D5, NOTE_F5, NOTE_G5, NOTE_A5,
-  NOTE_A5, NOTE_D5, NOTE_F5, NOTE_G5, NOTE_E5 
-  };
-
-  int tempo[]{
-  12, 12, 3,
-  12, 12, 3,
-  5, 10, 12, 12, 12, 12, 3,
-  6, 6, 12, 12, 2,
-  6, 6, 12, 12, 2,
-  12, 12, 3,
-  12, 12, 3,
-  5, 10, 12, 12, 12, 12, 3,
-  6, 6, 12, 12, 2,
-  6, 6, 12, 12, 2
-  };
-
-int songDuration = sizeof(melody);
-  for(int thisNote = 0;thisNote < songDuration; thisNote++){
-    if(buttonPressed == false && numSeconds >= numSecondsUntilBuzz){
-    int noteDuration = 1000/tempo[thisNote];
-    tone(buzzer,melody[thisNote],noteDuration);
-    int pauseBetweenNotes = noteDuration*1.80;
-    delay(pauseBetweenNotes);
-    }
-  }
-}
-
